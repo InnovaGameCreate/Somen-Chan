fa8c230edcdf669925c6410a36cdf41539539f6f
ryota100825 <ryota.game10@gmail.com>
2020-09-13 14:32:25 +0900

    ソーメンの動きを実装，ゲームスタートの動作の実装

---
+++ Assets/#MYASSETS/Scripts/Somen/BaseSomen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using Assets.Scripts.Somen.Inputs;
+
+namespace Assets.Scripts.Somen
+{
+    public abstract class BaseSomen : MonoBehaviour
+    {
+        protected SomenCore core;
+        protected Rigidbody somenRigidBody;
+        protected IInputEventProvider InputEvent;
+        private void Awake()
+        {
+            core = GetComponent<SomenCore>();
+            somenRigidBody = GetComponent<Rigidbody>();
+            InputEvent = GetComponent<IInputEventProvider>();
+            OnInitialize();
+        }
+        protected abstract void OnInitialize();
+    }
+}

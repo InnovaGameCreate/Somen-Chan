0b693d788f23238aca0fdfdd38103f3abb7d4867
ryota100825 <ryota.game10@gmail.com>
2020-09-14 15:42:44 +0900

    箸の動きのベースを作成

---
+++ Assets/#MYASSETS/Scripts/ChopStick/BaseChopStick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Assets.Scripts.ChopStick
+{
+    public abstract class BaseChopStick : MonoBehaviour
+    {
+        // 箸が壊れているかどうかのイベント
+        private ReactiveProperty<bool> isBroken = new ReactiveProperty<bool>(false);
+        public IReadOnlyReactiveProperty<bool> IsBroken { get { return isBroken; } }
+
+        // ソーメンを掴んでいるかどうかのイベント
+        private ReactiveProperty<bool> isGrab = new ReactiveProperty<bool>(false);
+        public IReadOnlyReactiveProperty<bool> IsGrab { get { return isGrab; } }
+
+        private void Awake()
+        {
+            OnInitializeChopStick();
+        }
+
+        /// <summary>
+        /// 掴むフラグをオンに
+        /// </summary>
+        public void SwitchOnIsGrab()
+        {
+            isGrab.Value = true;
+        }
+
+        protected abstract void OnInitializeChopStick();
+    }
+}
---
+++ Assets/#MYASSETS/Scripts/ChopStick/ChopStickAnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Assets.Scripts.ChopStick
+{
+    public class ChopStickAnimationController : BaseChopStick
+    {
+        protected override void OnInitializeChopStick()
+        {
+            Debug.Log("箸初期化");
+            // ソーメンを掴んだとき
+            IsGrab
+                .Where(isGrab => isGrab)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("衝突");
+                });
+
+            // 箸が壊れたとき
+            IsBroken
+                .Where(isBroken => isBroken)
+                .Subscribe(_ =>
+                {
+                });
+        }
+    }
+}

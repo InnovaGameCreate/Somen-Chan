f4bbd72300dd6fe9da72ff3910e0c31d75d7ddc5
ryota100825 <ryota.game10@gmail.com>
2020-09-14 17:27:52 +0900

    不要なUIを削除

--- Assets/#MYASSETS/Scripts/Somen/Input/InputAndroid.cs
+++ Assets/#MYASSETS/Scripts/Somen/Input/InputAndroid.cs
@@ -10,13 +10,6 @@ namespace Assets.Scripts.Somen.Inputs
 {
     public class InputAndroid : BaseSomen, IInputEventProvider
     {
-        [SerializeField]
-        private Text accelerationX = default;
-        [SerializeField]
-        private Text accelerationY = default;
-        [SerializeField]
-        private Text accelerationZ = default;
-
         private ReactiveProperty<Vector3> moveDirection = new ReactiveProperty<Vector3>();
         public IReadOnlyReactiveProperty<Vector3> MoveDirection { get { return moveDirection; } }
 
@@ -27,9 +20,6 @@ namespace Assets.Scripts.Somen.Inputs
                 .Subscribe(accelerationVector =>
                 {
                     moveDirection.Value = accelerationVector;
-                    accelerationX.text = "ジャイロX:" + accelerationVector.x;
-                    accelerationY.text = "ジャイロY:" + accelerationVector.y;
-                    accelerationZ.text = "ジャイロZ:" + accelerationVector.z;
                 });
         }
     }

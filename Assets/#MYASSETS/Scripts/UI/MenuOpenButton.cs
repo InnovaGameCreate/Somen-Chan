2296796c7d7a4980aa28d8f35df03b5ad0637945
ryota100825 <ryota.game10@gmail.com>
2020-09-16 18:02:21 +0900

    ゲームクリアとゲームオーバーのモーダルの追加

--- Assets/#MYASSETS/Scripts/UI/MenuOpenButton.cs
+++ Assets/#MYASSETS/Scripts/UI/MenuOpenButton.cs
@@ -1,15 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UniRx;
+using Assets.Scripts.ChopStick;
 
 public class MenuOpenButton : MonoBehaviour
 {
     [SerializeField]
     private ModalManagerPresenter modalPresenter = default;
+    [SerializeField]
+    private ChopStickProvider chopStickProvider = default;
+    private Button menuOpenButton;
+
+    private void Start()
+    {
+        menuOpenButton = GetComponent<Button>();
+
+        // ソーメンが掴まれると押せなくなる
+        chopStickProvider.IsGrab
+            .Subscribe(isGrab =>
+            {
+                if (isGrab)
+                {
+                    SetButtonInteractable(false);
+                }
+                else { SetButtonInteractable(true); }
+            });
+    }
 
     public void OnClick()
     {
         modalPresenter.SetIsPause(true);
+    }
+
+    /// <summary>
+    /// ボタンを押せるかどうかをセットする
+    /// </summary>
+    /// <param name="isInteractable">ボタンを押せるかどうか</param>
+    public void SetButtonInteractable(bool isInteractable)
+    {
+        menuOpenButton.interactable = isInteractable;
     }
 }
